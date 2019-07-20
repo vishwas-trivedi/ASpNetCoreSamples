62a442d687539dfd6813edf2ba65ca46334d46c1
Vishwas Trivedi <36479242+vishwas-trivedi@users.noreply.github.com>
2019-07-20 20:44:03 +0900

    ActionBlock sample
    Parallel task execution sample

---
+++ ParallelActionBlock/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ParallelActionBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("main start");
+            var program = new Program();
+            program.PerformCompute(5, 10);
+            Console.WriteLine("main end");
+        }
+        
+        public void PerformCompute(int maxParallalism, int executionCount)
+        {
+            var actionBlock = new ActionBlock<object[]>(DoSomething ,new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = maxParallalism });
+
+            for (var indx = 0; indx < executionCount; indx++)
+            {
+                actionBlock.Post(new object[] { indx });
+            }
+
+            actionBlock.Complete();
+            actionBlock.Completion.Wait();
+        }
+
+        public async Task DoSomething(object obj)
+        {
+            var data = obj as object[];
+            var n = (int)data[0];
+
+            Console.WriteLine($"Start : {n}");
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"[{n}] : [{i}]");
+            }
+            await Task.Delay(5000);
+            Console.WriteLine($"End : {n}");
+        }
+    }
+}
